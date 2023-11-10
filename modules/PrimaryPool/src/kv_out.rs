9b5894bc448b224a72f4869a647ed9a27c73c6fe
yaksheshguptac <yakshesh.gupta@credain.com>
2023-11-10 05:41:53 +0530

    added store for subscription event

--- modules/PrimaryPool/src/kv_out.rs
+++ modules/PrimaryPool/src/kv_out.rs
@@ -5,12 +5,11 @@ use substreams::store::{self, DeltaProto};
 use substreams_sink_kv::pb::sf::substreams::sink::kv::v1::KvOperations;
 // use substreams_sink_kv::pb::kv::KvOperations;
 use crate::pb;
-use crate::pb::verified::primary::v1::Pool;
-use pb::verified::primary::v1::Subscription;
+use crate::pb::verified::primary::v1::{Pool,Subscription, Subscriptions};
 
 // use crate::pb::eth::block_meta::v1::BlockMeta;
 
-pub fn process_deltas(ops: &mut KvOperations, deltas: store::Deltas<DeltaProto<Pool>>) {
+pub fn process_deltas(ops: &mut KvOperations, deltas: store::Deltas<DeltaProto<Subscription>>) {
     use substreams::pb::substreams::store_delta::Operation;
 
     for delta in deltas.deltas {
